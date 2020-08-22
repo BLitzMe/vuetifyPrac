388b3e75f30b2af65d49fe168466526f7f57c414
umershakoor <veryintuitive1@gmail.com>
2020-08-22 22:31:43 +0200

    flex and containers etc

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  vuetify,
+  // eslint-disable-next-line prettier/prettier
+  render: (h) => h(App)
 }).$mount("#app");
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subtitle-1 grey--text">Dashboard</h1>
+    <v-container class="my-5"
+      ><v-card flat outlined class="pa-3">
+        <v-layout row wrap class="pa-3">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>create a new website</div>
+          </v-flex>
+          <v-flex caption xs6 sm4 md2
+            >Person
+            <div>Holy Moly</div></v-flex
+          >
+          <v-flex caption xs6 sm4 md2
+            >Due By
+            <div>day after tomorrow</div></v-flex
+          >
+          <v-flex caption xs2 sm4 md2
+            >Status
+            <div>Ongoing</div></v-flex
+          >
+        </v-layout>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
